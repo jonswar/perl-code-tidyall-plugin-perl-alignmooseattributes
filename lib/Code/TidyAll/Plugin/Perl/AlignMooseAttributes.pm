d19ba3d1ee6dea8ce27c66cc7355921ad452dacb
Jonathan Swartz <swartz@pobox.com>
2012-07-22 08:16:15 -0700

    import

--- lib/Code/TidyAll/Plugin/Perl/AlignMooseAttributes.pm
+++ lib/Code/TidyAll/Plugin/Perl/AlignMooseAttributes.pm
@@ -1,7 +1,47 @@
 package Code::TidyAll::Plugin::Perl::AlignMooseAttributes;
-use 5.008;
+use Text::Aligner qw(align);
+use List::Util qw(max);
 use strict;
 use warnings;
+use base qw(Code::TidyAll::Plugin);
+
+my $marker = '__AlignMooseAttributes_no_tidy';
+
+sub preprocess_source {
+    my ( $self, $source ) = @_;
+
+    # Hide 'has' one-liners behind comments
+    $source =~ s/^( has .* \) \s* \; )$/\# $marker $1/gmx;
+
+    return $source;
+}
+
+sub postprocess_source {
+    my ( $self, $source ) = @_;
+
+    # Reveal 'has' lines
+    $source =~ s/^\# $marker //gm;
+
+    # For each group of 'has' one-liners: sort, remove multiple spaces and align equal signs
+    $source =~ s/((^ has .*? \) \s* \; \s* \n)+)/process_attr_block($1)/gmex;
+
+    return $source;
+}
+
+sub process_attr_block {
+    my ($block) = @_;
+    my @lines = sort( grep { /\S/ } split( "\n", $block ) );
+    my @attrs = map { /has '([^\']+)'/; $1 } @lines;
+    my $max_length = max( map { length($_) } @attrs );
+    foreach my $line (@lines) {
+        $line =~ s/  +/ /g;
+        $line =~
+          s/(?:has \s+ '([^\']+)') \s* => \s* \( \s* (.*?) \s* \) \s* ;/"has '$1'" . scalar(' ' x ($max_length - length($1))) . " => ( $2 );"/ex;
+        $line =~ s/=> \(\s*\)/=> \(\)/;
+        $line =~ s/,\s+\)/ \)/;
+    }
+    return join( "", map { "$_\n" } @lines ) . "\n";
+}
 
 1;
 
@@ -11,7 +51,8 @@ __END__
 
 =head1 NAME
 
-Code::TidyAll::Plugin::Perl::AlignMooseAttributes - TidyAll plugin to sort and align Moose-style attributes
+Code::TidyAll::Plugin::Perl::AlignMooseAttributes - Sort and align Moose-style
+attributes with tidyall
 
 =head1 SYNOPSIS
 
@@ -19,11 +60,32 @@ Code::TidyAll::Plugin::Perl::AlignMooseAttributes - TidyAll plugin to sort and a
 
 =head1 DESCRIPTION
 
-Code::TidyAll::Plugin::Perl::AlignMooseAttributes provides
+This L<tidyall|tidyall> plugin sorts and aligns consecutive Moose-style
+attribute lines. e.g. this:
 
-Questions and feedback are welcome, and should be directed to the mailing list:
+    has 'namespace' => ( is => 'ro', isa => 'Str', default => 'Default' );
+    has 'expires_at' => ( is => 'rw', default => CHI_Max_Time );
+    has 'storage' => ( is => 'ro' );
+    has 'label' => ( is => 'rw', lazy_build => 1 );
+    has 'chi_root_class' => ( is => 'ro' );
 
-    http://groups.google.com/...
+becomes this:
+
+    has 'chi_root_class' => ( is => 'ro' );
+    has 'expires_at'     => ( is => 'rw', default => CHI_Max_Time );
+    has 'label'          => ( is => 'rw', lazy_build => 1 );
+    has 'namespace'      => ( is => 'ro', isa => 'Str', default => 'Default' );
+    has 'storage'        => ( is => 'ro' );
+
+Only consecutive attributes, each on a single line, will be affected.
+Multi-line attributes will not be affected.
+
+This plugin has a preprocess step that hides these lines to prevent perltidy
+from splitting them into multiple lines.
+
+=head1 SUPPORT AND DOCUMENTATION
+
+Questions and feedback are welcome, and should be directed to the author.
 
 Bugs and feature requests will be tracked at RT:
 
@@ -37,4 +99,5 @@ The latest source code can be browsed and fetched at:
 
 =head1 SEE ALSO
 
-L<Some::Module>
+L<perltidy|perltidy>
+
