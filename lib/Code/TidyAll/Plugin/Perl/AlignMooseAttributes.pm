773741eac538cd6a3d8bb65bc331e590a2d94022
Jonathan Swartz <swartz@pobox.com>
2012-07-22 07:40:51 -0700

    init

---
+++ lib/Code/TidyAll/Plugin/Perl/AlignMooseAttributes.pm
@@ -0,0 +1,40 @@
+package Code::TidyAll::Plugin::Perl::AlignMooseAttributes;
+use 5.008;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Code::TidyAll::Plugin::Perl::AlignMooseAttributes - TidyAll plugin to sort and align Moose-style attributes
+
+=head1 SYNOPSIS
+
+    use Code::TidyAll::Plugin::Perl::AlignMooseAttributes;
+
+=head1 DESCRIPTION
+
+Code::TidyAll::Plugin::Perl::AlignMooseAttributes provides
+
+Questions and feedback are welcome, and should be directed to the mailing list:
+
+    http://groups.google.com/...
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code-TidyAll-Plugin-Perl-AlignMooseAttributes
+    bug-code-tidyall-plugin-perl-alignmooseattributes@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-code-tidyall-plugin-perl-alignmooseattributes
+    git clone git://github.com/jonswar/perl-code-tidyall-plugin-perl-alignmooseattributes.git
+
+=head1 SEE ALSO
+
+L<Some::Module>
